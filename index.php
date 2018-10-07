e663e27563d97d14208c8892ba03897a7b62e4c9
Iván Quesada <32323558+ivanquesadapalmero@users.noreply.github.com>
2018-10-07 13:28:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -81,8 +81,8 @@ if (isset($_POST['borrar'])) {
                 print"<tr>
                 <td>".$producto['nombre']."</td>
                 <td>".$producto['cantidad']."</td>
-                <td>".$producto['precio']."</td>
-                <td>".$producto['total']."</td>
+                <td>".$producto['precio']."€</td>
+                <td>".$producto['total']."€</td>
                 </tr>";
                 
             }
